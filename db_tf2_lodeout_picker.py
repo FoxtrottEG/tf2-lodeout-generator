049cb3f9994f58353136eaad6752898b7e2cd2b7
Foxtrott <106594957+FoxtrottEG@users.noreply.github.com>
2025-11-27 10:17:39 +0100

    addes fuck ass db
    added compleated db for primary weapons
    also contains scraps of Side arm and Anti tank lists
    
    back when i wanted to use lists for all this shit 2 hours ago...

---
+++ db_tf2_lodeout_picker.py
@@ -0,0 +1,126 @@
+# upgrades primary
+
+SCOPES = [
+    "Holographic Sight",
+    "HCOG Sight",
+    "Threat Scope (Digital/Thermal)",
+    "AOG Scope (Mid-range)",
+    "Default Iron Sights"
+]
+
+SNIPER_SCOPES = [
+    "Variable Zoom Scope",
+    "HCOG Sight",
+    "Threat Scope (Digital/Thermal)",
+    "Default Iron Sights"
+]
+
+UPGRADES = [
+    "Extended Magazine",
+    "Speedloader",
+    "Gunrunner",
+    "Quick Swap",
+    "Extra Ammo"
+]
+
+PR_WEAPON = {
+    # ar
+    "R-201 Carbine": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Hemlok BF-R": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "G2A5": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+
+    # smgs
+    "C.A.R. SMG": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Volt SMG": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "R-97 Compact SMG": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Alternator SMG": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+
+    # lmgs
+    "Spitfire": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "L-STAR": {
+        "scopes": SCOPES,
+        "upgrades": ["Extended Magazine", "Speedloader", "Gunrunner", "Fast Reload"]
+    },
+    "Devotion": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+
+    # shotguns
+    "EVA-8 Auto": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Mastiff Shotgun": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+
+    # snipers
+    "Kraber-AP": {
+        "scopes": SNIPER_SCOPES,
+        "upgrades": ["Extended Magazine", "Speedloader", "Extra Ammo", "Ricochet"]
+    },
+    "Double Take": {
+        "scopes": SNIPER_SCOPES,
+        "upgrades": ["Extended Magazine", "Speedloader", "Extra Ammo", "Ablative Rounds"]
+    },
+    "Longbow-DMR": {
+        "scopes": SCOPES + ["Variable Zoom Scope"],
+        "upgrades": UPGRADES
+    },
+
+    # grens
+    "SMR (Sidewinder)": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Cold War": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Softball": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+    "Epg-1": {
+        "scopes": SCOPES,
+        "upgrades": UPGRADES
+    },
+}
+# soon to be sidearm dick
+SE_WEAPON = [
+    'sweapon1',
+    'sweapon2',
+    'sweapon3'
+]
+# soon to be at guns dick
+AT_WEAPON = [
+    'atweapon1',
+    'atweapon2',
+    'atweapon3'
+]
