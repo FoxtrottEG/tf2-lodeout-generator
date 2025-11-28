9b5d1ce93ba31317cf53c96ec90dc8138d86256c
Foxtrott <106594957+FoxtrottEG@users.noreply.github.com>
2025-11-28 10:11:34 +0100

    Added titans
    program now randomly generates a titan 
    full support for monarch core upgrades

--- tf2_lodeout_picker.py
+++ tf2_lodeout_picker.py
@@ -1,5 +1,5 @@
 import random
-from db_tf2_lodeout_picker import (TACTICAL, PR_WEAPON, SA_WEAPON, AT_WEAPON, BOOSTS, ORDINANCE, KIT1, KIT2)
+from db_tf2_lodeout_picker import (TITANS,TACTICAL, PR_WEAPON, SA_WEAPON, AT_WEAPON, BOOSTS, ORDINANCE, KIT1, KIT2)
 
 # select pilot kit
 tact = random.choice(TACTICAL)
@@ -12,12 +12,9 @@ PR_WEAPONS_DETAILS = (PR_WEAPON[prw])
 SCOPE_List = PR_WEAPONS_DETAILS['scopes']
 prw_sc = random.choice(SCOPE_List)
 
-PR_WEAPONS_DETAILS = (PR_WEAPON[prw])
 UPGRADES_List = PR_WEAPONS_DETAILS['upgrades']
 prw_up1 = random.choice(UPGRADES_List)
 UPGRADES_List.remove(prw_up1)
-
-UPGRADES_List = PR_WEAPONS_DETAILS['upgrades']
 prw_up2 = random.choice(UPGRADES_List)
 
 # selection secoundary
@@ -28,8 +25,6 @@ SA_WEAPONS_DETAILS = (SA_WEAPON[saw])
 UPGRADES_List = SA_WEAPONS_DETAILS['upgrades']
 saw_up1 = random.choice(UPGRADES_List)
 UPGRADES_List.remove(saw_up1)
-
-UPGRADES_List = SA_WEAPONS_DETAILS['upgrades']
 saw_up2 = random.choice(UPGRADES_List)
 
 # selection anti-titan
@@ -40,8 +35,6 @@ AT_WEAPONS_DETAILS = (AT_WEAPON[atw])
 UPGRADES_List = AT_WEAPONS_DETAILS['upgrades']
 atw_up1 = random.choice(UPGRADES_List)
 UPGRADES_List.remove(atw_up1)
-
-UPGRADES_List = AT_WEAPONS_DETAILS['upgrades']
 atw_up2 = random.choice(UPGRADES_List)
 
 # selects ordinance
@@ -53,6 +46,28 @@ boost = random.choice(BOOSTS)
 # select kits
 kit1 = random.choice(KIT1)
 kit2 = random.choice(KIT2)
+
+# select titan
+TITANS_NAMES = list(TITANS.keys())
+Titan = random.choice(TITANS_NAMES)
+
+TITANS_DETAILS = (TITANS[Titan])
+titan_kit = TITANS_DETAILS["titan_kit"]
+titankit = random.choice(titan_kit)
+
+titanfall_kit = TITANS_DETAILS["titanfall_kit"]
+titanfallkit = random.choice(titanfall_kit)
+
+spectitankit = TITANS_DETAILS["special_kit"]
+titspec1 = random.choice(spectitankit)
+
+if Titan == "Monarch":
+    monarchkit1 = TITANS_DETAILS["monarch_core1"]
+    monarch_kit1 = random.choice(monarchkit1)
+    monarchkit2 = TITANS_DETAILS["monarch_core2"]
+    monarch_kit2 = random.choice(monarchkit2)
+    monarchkit3 = TITANS_DETAILS["monarch_core3"]
+    monarch_kit3 = random.choice(monarchkit3)
 
 print(f"  === Tactical ===")
 print(f"Pilot-Tactical: {tact}")
@@ -82,3 +97,16 @@ print(f"Boost: {boost}")
 print(f"  === Kits ===")
 print(f"Kit-1: {kit1}")
 print(f"Kit-2:{kit2}")
+
+print(f"  === Titan ===")
+print(f"Titan: {Titan}")
+print(f"Titan Kit : {titankit}")
+print(f"Titanfall Kit : {titanfallkit}")
+print(f"{Titan} Kit :  {titspec1}")
+
+if Titan == "Monarch":
+    print(f" === Monarch Kits ===")
+    print(f"Monarch kit 1 : {monarch_kit1}")
+    print(f"Monarch kit 2 : {monarch_kit2}")
+    print(f"Monarch kit 3 : {monarch_kit3}")
+  
